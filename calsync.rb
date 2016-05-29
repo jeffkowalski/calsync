bb24d1183f3e84c3072ec759c0e165068451653a
Jeff Kowalski <jeff.kowalski@gmail.com>
2016-05-29 11:16:24 -0700

    Add test to count folder sync items

--- calsync.rb
+++ calsync.rb
@@ -4,8 +4,10 @@ require 'viewpoint'
 include Viewpoint::EWS
 
 require 'json'
+require 'andand'
+require 'awesome_print'
 
-#CREDENTIALS_PATH = File.join(Dir.home, '.credentials', "calsync.json")
+
 CREDENTIALS_PATH = File.join('.', '.credentials', "calsync.json")
 
 class Z
@@ -76,7 +78,7 @@ end
 def test0
   calendar = @client.get_folder :calendar
   events   = calendar.items
-  [events.last].each { |event|
+  [events.first].each { |event|
     puts event.subject
     # puts event.start_time
     # puts event.end_time
@@ -90,11 +92,9 @@ def test1
   folders.each { |f| puts f.name }
 end
 
-require "awesome_print"
-
 def dump_item item
   puts "Id:         #{item.id}"
-  #puts "ItemId:     #{item.item_id}" # contains id and change_key
+  puts "ItemId:     #{item.item_id}" # contains id and change_key
   puts "Subject:    #{item.subject}"
   puts "Type:       #{item.ews_item[:calendar_item_type][:text]}"  # Single or RecurringMaster
 #  ap item.ews_item  # complete record
@@ -102,8 +102,10 @@ def dump_item item
   puts "End:        #{item.end}"
   puts "Location:   #{item.location}"
   puts "Organizer:  #{item.organizer.name}"
-#  puts "OptionalAttendees: #{item.optional_attendees}"
-#  puts "RequiredAttendees: #{item.required_attendees}"
+  puts "OptionalAttendees:"
+  ap item.optional_attendees #returns Array of MailboxUser
+  puts "RequiredAttendees:"
+  ap item.required_attendees.map { |attendee| attendee.name } #returns Array of MailboxUser
   puts "Recurring:  #{item.recurring?}"
   puts "Recurrence: #{item.recurrence}"
   puts "ChangeKey:  #{item.change_key}"
@@ -159,20 +161,15 @@ def test5
   # http://www.rubydoc.info/github/WinRb/Viewpoint/Viewpoint/EWS/Types/GenericFolder#subscribe-instance_method
 end
 
+def test6
+  calendar = @client.get_folder :calendar
+  puts "got calendar"
+  while not calendar.synced?
+    result = calendar.sync_items!
+    [:create, :update, :delete, :read_flag_change].each { |flag|
+      puts "#{flag}\t#{result[flag].andand.count}" unless result[flag].nil?
+    }
+  end
+end
+
 test5
-
-# maxnum=512
-# opts[:calendar_view] = {:max_entries_returned => maxnum, :start_date => start_date, :end_date => end_date}
-# calfolder.find_items(#opts
-
-
-
-# You can achieve this by 'required_attendees' & 'optional_attendees' methods on calendar_item
-# @calendar = cli.get_folder :calendar:
-#                              @all_events = @calendar.items
-# first_item = @all_events.first
-
-# required_attendees = first_item.required_attendees #returns Array of MailboxUser
-# optional_attendees = first_item.optional_attendees #returns Array of MailboxUser
-
-# Ref following commit patch which adds response type for each attendee #pramodshinde@c08113a
